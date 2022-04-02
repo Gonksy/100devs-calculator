4c58f2587afac2e8412d7d8f4aa96dfb56269ab1
Ahmad Yosry <realbulletdragon@gmail.com>
2022-04-02 05:09:49 +0200

    Added html and some css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-
+//Calculator Object > Methods Object > Buttons object, Display object
+//Buttons object: 10 number inputs, 4 binary operators, equal
+//Display object: When user hits button: Wipes display, shows user input.
+//                When user hits equal: Wipes display, shows ouput of equation.
