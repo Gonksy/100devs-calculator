80389012c0a74b08d696386c5ae5660f7ef6ad6f
Ahmad Yosry <realbulletdragon@gmail.com>
2022-04-02 18:58:22 +0200

    Basic logic applied.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@
 //Buttons object: 10 number inputs, 4 binary operators, equal
 //Display object: When user hits button: Wipes display, shows user input.
 //                When user hits equal: Wipes display, shows ouput of equation.
+
+
+function Calculator(str) {
+  this.methods = {
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b,
+    '/': (a, b) => a + b,
+    '+': (a, b) => a + b,
+  }
+
+  this.calculate = (str) => {
+    let tempArray = str.split(' ')
+    let a = tempArray[0]
+    let op = tempArray[1]
+    let b = tempArray[2]
+    return this.methods[op](a, b)
+  }
+
+}
+
